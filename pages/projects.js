ba1d838ef402797dad4953d76569aa053f9e2cb1
Hunter Germundsen <hbwgermundsen@gmail.com>
2022-06-13 16:35:10 -0400

    Added Navbar component along with about, contact, and projects pages

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 
-export default function Home() {
-  return (
+const Projects = () => {
+  return ( 
     <div>
       <Navbar />
-      <h1>Welcome, you found me!</h1>
+      <h1>Projects</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus omnis magni nobis harum ut, eaque saepe eveniet obcaecati ex vero accusantium maiores esse molestiae illum facere necessitatibus ab? Ratione.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus omnis magni nobis harum ut, eaque saepe eveniet obcaecati ex vero accusantium maiores esse molestiae illum facere necessitatibus ab? Ratione.</p>
     </div>
-  )
+   );
 }
+ 
+export default Projects;
