e58a32b69eccb4ef45d2d67a69b1f49131cd5501
Hunter Germundsen <hbwgermundsen@gmail.com>
2022-06-13 16:43:23 -0400

    Added Layout component to include navbar on all pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,6 @@
-import Navbar from "../components/Navbar";
-
 const Contact = () => {
   return ( 
     <div>
-      <Navbar />
       <h1>Contact</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus omnis magni nobis harum ut, eaque saepe eveniet obcaecati ex vero accusantium maiores esse molestiae illum facere necessitatibus ab? Ratione.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus omnis magni nobis harum ut, eaque saepe eveniet obcaecati ex vero accusantium maiores esse molestiae illum facere necessitatibus ab? Ratione.</p>
--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,6 @@
-import Navbar from "../components/Navbar";
-
 const Projects = () => {
   return ( 
     <div>
-      <Navbar />
       <h1>Projects</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus omnis magni nobis harum ut, eaque saepe eveniet obcaecati ex vero accusantium maiores esse molestiae illum facere necessitatibus ab? Ratione.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus omnis magni nobis harum ut, eaque saepe eveniet obcaecati ex vero accusantium maiores esse molestiae illum facere necessitatibus ab? Ratione.</p>
